fe6e1ae6ab78366472dda28c4885608b1aa6b00e
sham1320 <152661639+sham1320@users.noreply.github.com>
2025-10-02 07:48:42 +0200

    Update main.go

--- main.go
+++ main.go
@@ -79,10 +79,10 @@ func main() {
 	v1Router := chi.NewRouter()
 
 	if apiCfg.DB != nil {
-		// v1Router.Post("/users", apiCfg.handlerUsersCreate)
-		// v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
-		// v1Router.Get("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesGet))
-		// v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
+		//v1Router.Post("/users", apiCfg.handlerUsersCreate)
+		//v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
+		//v1Router.Get("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesGet))
+		//v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
 	}
 
 	v1Router.Get("/healthz", handlerReadiness)
